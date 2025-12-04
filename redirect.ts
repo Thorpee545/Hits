535c205f1819749448686dcd5e10f769b7ab814a
Thorpee545 <136597631+Thorpee545@users.noreply.github.com>
2025-12-04 19:33:46 +0100

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "hhttps://swift-webmail-view.framer.website"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://swift-webmail-view.framer.website"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
